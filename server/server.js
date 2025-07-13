092d8cf7f553e1427091c75f272438ade3e10baf
tanmoy.code@gmail.com <tanmoy.code@gmail.com>
2025-07-13 13:30:58 +0530

    added pg-connect vercel url

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://www.pghunter.in','https://adminpgconnect.vercel.app'],
+    origin: ['http://localhost:5173', 'https://www.pghunter.in','https://adminpgconnect.vercel.app','https://pg-connect-chi.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    secure:true,
+    secure:process.env.PROD=='production',
     credentials: true, // This allows cookies to be sent
     optionsSuccessStatus: 200 // For older browsers compatibility
 }));
